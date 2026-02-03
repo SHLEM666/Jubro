60e862628fe44e466428a0bbe3387de4bc45aa3a
SHLEM666 <shlem666@yandex.ru>
2026-02-03 10:33:04 +0300

    Add JubroIcons object

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 
 //    api(libs.androidx.compose.foundation)
 //    api(libs.androidx.compose.foundation.layout)
-//    api(libs.androidx.compose.material.iconsExtended)
+    api(libs.androidx.compose.material.iconsExtended)
     api(libs.androidx.compose.material3)
     api(libs.androidx.compose.material3.adaptive)
     api(libs.androidx.compose.material3.navigationSuite)
---
+++ core/designsystem/src/main/kotlin/com/shlem666/jubro/core/designsystem/icon/JubroIcons.kt
@@ -0,0 +1,22 @@
+package com.shlem666.jubro.core.designsystem.icon
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.automirrored.filled.Redo
+import androidx.compose.material.icons.automirrored.filled.Undo
+import androidx.compose.material.icons.automirrored.outlined.ViewSidebar
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Refresh
+
+object JubroIcons {
+    val ViewSidebar = Icons.AutoMirrored.Outlined.ViewSidebar
+    val Refresh = Icons.Filled.Refresh
+    val Undo = Icons.AutoMirrored.Filled.Undo
+    val Redo = Icons.AutoMirrored.Filled.Redo
+    val KeyboardArrowLeft = Icons.AutoMirrored.Filled.KeyboardArrowLeft
+    val KeyboardArrowRight = Icons.AutoMirrored.Filled.KeyboardArrowRight
+    val KeyboardArrowUp = Icons.Filled.KeyboardArrowUp
+    val KeyboardArrowDown = Icons.Filled.KeyboardArrowDown
+}
