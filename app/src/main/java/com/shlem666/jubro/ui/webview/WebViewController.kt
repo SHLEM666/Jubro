0af69d6cecc536dfd8ca372f1baa3f1de13b4fae
SHLEM666 <shlem666@yandex.ru>
2026-03-25 10:17:38 +0300

    Add keyboard shortcut support to the simulateKeyPress method of the WebViewController class

--- app/src/main/java/com/shlem666/jubro/ui/webview/WebViewController.kt
+++ app/src/main/java/com/shlem666/jubro/ui/webview/WebViewController.kt
@@ -1,9 +1,11 @@
 package com.shlem666.jubro.ui.webview
 
+import android.os.SystemClock
+import android.view.InputDevice
 import android.view.KeyEvent
 import android.webkit.WebView
-import com.shlem666.jubro.core.data.repository.JsDataRepository
 import javax.inject.Inject
+import com.shlem666.jubro.core.data.repository.JsDataRepository
 
 class WebViewController @Inject constructor(
     private val jsDataRepository: JsDataRepository
@@ -17,8 +19,39 @@ class WebViewController @Inject constructor(
         )
     }
 
-    fun simulateKeyPress(code: Int) {
-        webView.dispatchKeyEvent(KeyEvent(KeyEvent.ACTION_DOWN, code))
-        webView.dispatchKeyEvent(KeyEvent(KeyEvent.ACTION_UP, code))
+    fun simulateKeyPress(
+        code: Int,
+        metaStateCode1: Int = 0,
+        metaStateCode2: Int = 0
+    ) {
+        val eventTime = SystemClock.uptimeMillis()
+        webView.dispatchKeyEvent(
+            KeyEvent(
+                eventTime,
+                eventTime,
+                KeyEvent.ACTION_DOWN,
+                code,
+                0,
+                metaStateCode1 or metaStateCode2,
+                0,
+                0,
+                0,
+                InputDevice.SOURCE_KEYBOARD
+            )
+        )
+        webView.dispatchKeyEvent(
+            KeyEvent(
+                eventTime + 50,
+                eventTime + 50,
+                KeyEvent.ACTION_UP,
+                code,
+                0,
+                metaStateCode1 or metaStateCode2,
+                0,
+                0,
+                0,
+                InputDevice.SOURCE_KEYBOARD
+            )
+        )
     }
 }
