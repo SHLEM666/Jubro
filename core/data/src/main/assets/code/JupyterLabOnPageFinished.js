5f046e6e9011d93f03b1ecc0318ae56258634d8b
SHLEM666 <shlem666@yandex.ru>
2026-06-10 16:39:30 +0300

    Modify JupyterLabOnPageFinished script: - Add command to remove unnecessary pudding - Commenting and refactoring

--- core/data/src/main/assets/code/JupyterLabOnPageFinished.js
+++ core/data/src/main/assets/code/JupyterLabOnPageFinished.js
@@ -3,25 +3,41 @@
 	if (document.body) {
 		try {
 			var elem = document.createElement("style");
+
+			// Make main menu panel scrollable
 			elem.innerHTML += "#jp-menu-panel {overflow-x: scroll;}";
+
+			// Remove unnecessary padding from the main menu panel
+			// that appear on the left in front of the File
+			// menu button in the Simple interface mode.
 			elem.innerHTML += ".jp-LabShell[data-shell-mode='single-document'] #jp-menu-panel {padding: 0px;}";
+
+            // Remove unnecessary padding that appears as the thin grey
+            // frame around workspace in default interface mode.
+            elem.innerHTML += "#jp-main-dock-panel {padding: 0px;}";
+
             //elem.innerHTML += "#jp-MainMenu {overflow-x: scroll;}";
 			//elem.innerHTML += ".jp-NotebookTrustedStatus {display: none;}";
 			//elem.innerHTML += "select {display: none;}";
+
 			document.body.appendChild(elem);
+
+			// Cancel the context menu event in the code editing area
 			document.addEventListener('contextmenu', (e) => {
 				if (
+                    e.target.closest('.jp-Cell')
+
 				    //e.target.closest('.jp-CodeMirrorEditor') ||
 				    //e.target.closest('.jp-RenderedMarkdown') ||
                     //e.target.closest('.jp-RenderedText') ||
                     //e.target.closest('.jp-OutputArea') ||
                     //e.target.closest('.jp-InputArea') ||
-                    e.target.closest('.jp-Cell')
 				) {
 					e.stopImmediatePropagation();
 					e.stopPropagation();
 				}
 			}, true);
+
 		} catch (err) {
 			alert(err);
 		};
