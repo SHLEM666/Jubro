53ec9f40ec808fabba69032f15e04c22557d46f6
SHLEM666 <shlem666@yandex.ru>
2026-09-10 23:31:04 +0300

    Update JupyterLabOnPageFinished.js - Make the contextmenu event canceling unconditional - Remove commented lines of code

--- core/data/src/main/assets/code/JupyterLabOnPageFinished.js
+++ core/data/src/main/assets/code/JupyterLabOnPageFinished.js
@@ -1,5 +1,4 @@
 (function (window) {
-	//if (/.ipynb/.test(window.location.href)) {
 	if (document.body) {
 		try {
 			var elem = document.createElement("style");
@@ -23,26 +22,12 @@
             // frame around workspace in default interface mode.
             elem.innerHTML += "#jp-main-dock-panel {padding: 0px;}";
 
-            //elem.innerHTML += "#jp-MainMenu {overflow-x: scroll;}";
-			//elem.innerHTML += ".jp-NotebookTrustedStatus {display: none;}";
-			//elem.innerHTML += "select {display: none;}";
-
 			document.body.appendChild(elem);
 
-			// Cancel the context menu event in the code editing area
+			// Cancel the context menu event
 			document.addEventListener('contextmenu', (e) => {
-				if (
-                    e.target.closest('.jp-Cell')
-
-				    //e.target.closest('.jp-CodeMirrorEditor') ||
-				    //e.target.closest('.jp-RenderedMarkdown') ||
-                    //e.target.closest('.jp-RenderedText') ||
-                    //e.target.closest('.jp-OutputArea') ||
-                    //e.target.closest('.jp-InputArea') ||
-				) {
-					e.stopImmediatePropagation();
-					e.stopPropagation();
-				}
+				e.stopImmediatePropagation();
+				e.stopPropagation();
 			}, true);
 
 		} catch (err) {
